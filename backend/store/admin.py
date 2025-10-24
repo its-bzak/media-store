59c0ee6cf3003d0692d8e5bb451c108e8f9e2811
Ryan B <barczakryan@outlook.com>
2025-10-24 18:29:30 -0400

    Added Cart and CartItem to admin.py

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Media, Customer, Order, OrderItem
+from .models import Media, Customer, Order, OrderItem, Cart, CartItem
 # Register your models here.
 
 admin.site.register(Media)
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
