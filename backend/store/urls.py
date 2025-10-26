54bbabb198732dcb009de352ca8ef1742da81e0f
Ryan B <barczakryan@outlook.com>
2025-10-26 16:37:58 -0400

    Resolved bug that caused 404 error upon attempting to register a new user

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.media_list, name='media_list'),       # /media/
-    path('about/', views.about, name='about'),           # /media/about/
-    path('cart/', views.view_cart, name='view_cart'),     # /media/cart/
-    path('<slug:slug>/', views.item_info, name='item_info'),  # /media/<slug>/
-    path('register/', views.register, name='register'),
+    path('', views.media_list, name='media_list'),       # /store/
+    path('about/', views.about, name='about'),           # /store/about/
+    path('cart/', views.view_cart, name='view_cart'),     # /store/cart/
+    path('register/', views.register, name='register'), # / store/register/
+    path('<slug:slug>/', views.item_info, name='item_info'),  # /store/<slug>/
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -49,7 +49,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('media_list')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
